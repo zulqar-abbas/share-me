5e675e81d529210388d22b9365301248fbd02223
Zulqarnain Abbas <zulqarnain.abbas@conradlabs.com>
2022-11-11 04:51:12 +0500

    fix login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Home from "./containers/Home";
+import { loggedInUser } from "./utils/fetchUser";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = loggedInUser();
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
